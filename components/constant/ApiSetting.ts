ebd4ce853e204a5fb1ad1ed783c6943d9c7f7ad4
harry.chan <harry.chan@speedylinkage.com>
2024-05-08 17:42:52 +0800

    Api URL update

--- components/constant/ApiSetting.ts
+++ components/constant/ApiSetting.ts
@@ -5,8 +5,8 @@ const apiURL = 'https://canine-shelter-dog-api.zeabur.app/'; // zeabur
 // const apiURL = 'http://192.168.10.56:8000/'; // Office Local Lan IP
 // const apiURL = 'http://localhost:8000/'; // localhost
 
-// const wsURL = 'ws://canine-shelter-dog-api.zeabur.app:10000/' // zeabur websocket
-const wsURL = 'ws://192.168.10.56:10000/'; // Office Local Lan websocket
+const wsURL = 'ws://canine-shelter-dog-api.zeabur.app:10000/'; // zeabur websocket
+// const wsURL = 'ws://192.168.10.56:10000/'; // Office Local Lan websocket
 // const wsURL = 'ws://localhost:10000/'; // localhost websocket
 
 const axiosSetting: AxiosRequestConfig = {
