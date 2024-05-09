c1bc08e23684a5b860983a3987a85577018597ad
harry.chan <harry.chan@speedylinkage.com>
2024-05-09 10:11:52 +0800

    Remove port

--- components/constant/ApiSetting.ts
+++ components/constant/ApiSetting.ts
@@ -1,13 +1,13 @@
 import { AxiosRequestConfig } from 'axios';
 
-// const apiURL = 'https://canine-shelter-dog-api.zeabur.app/'; // zeabur
+const apiURL = 'https://canine-shelter-dog-api.zeabur.app/'; // zeabur
 // const apiURL = 'http://192.168.1.8:8000/'; // Home Local Lan IP
 // const apiURL = 'http://192.168.10.56:8000/'; // Office Local Lan IP
-const apiURL = 'http://localhost:8000/'; // localhost
+// const apiURL = 'http://localhost:8000/'; // localhost
 
-// const wsURL = 'ws://canine-shelter-dog-websocket.zeabur.app:10000/' // zeabur websocket
+const wsURL = 'wss://canine-shelter-dog-websocket.zeabur.app/'; // zeabur websocket
 // const wsURL = 'ws://192.168.10.56:10000/'; // Office Local Lan websocket
-const wsURL = 'ws://localhost:10000/'; // localhost websocket
+// const wsURL = 'ws://localhost:10000/'; // localhost websocket
 
 const axiosSetting: AxiosRequestConfig = {
     baseURL: apiURL,
